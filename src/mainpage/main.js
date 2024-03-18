446dc8b5d5a10f8916445ef20af0fa544c3bc1a0
김민준 <hanleejss012@naver.com>
2024-03-19 01:51:28 +0900

    swiper 활성

--- src/mainpage/main.js
+++ src/mainpage/main.js
@@ -1,32 +1,37 @@
 import React from 'react';
 import './main.css';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay } from 'swiper/modules';
+import { Autoplay, Navigation} from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/autoplay';
+import 'swiper/css/navigation';
 
 function Main() {
     return (
         <main id="contents">
             <section className="swiper">
                 <Swiper
-                    modules={[Autoplay]}
+                    modules={[Autoplay, Navigation]}
                     spaceBetween={50}
-                    slidesPerView={5}
-                    onSlideChange={() => console.log('slide change')}
-                    onSwiper={(swiper) => console.log(swiper)}
-                    autoplay={{ delay: 2000, disableOnInteraction: false }}
+                    slidesPerView={6}
+                    navigation
+                    centeredSlides={true} // 슬라이드를 중앙 정렬
+                    centeredSlidesBounds={true} // 중앙 정렬된 슬라이드가 뷰포트 경계에 도달할 때까지 슬라이드 정렬
+                    onSlideChange={() => {}}
+                    onSwiper={(swiper) => {}}
+                    autoplay={{ delay: 3000, disableOnInteraction: false }}
                     loop={true}
-                >
-                    <SwiperSlide>Slide 1</SwiperSlide>
-                    <SwiperSlide>Slide 2</SwiperSlide>
-                    <SwiperSlide>Slide 3</SwiperSlide>
-                    <SwiperSlide>Slide 4</SwiperSlide>
-                    <SwiperSlide>Slide 5</SwiperSlide>
-                    <SwiperSlide>Slide 6</SwiperSlide>
-                    <SwiperSlide>Slide 7</SwiperSlide>
-                    <SwiperSlide>Slide 8</SwiperSlide>
-                    <SwiperSlide>Slide 9</SwiperSlide>
+                >        
+                        <SwiperSlide>0</SwiperSlide>
+                        <SwiperSlide>1</SwiperSlide>
+                        <SwiperSlide>2</SwiperSlide>
+                        <SwiperSlide>3</SwiperSlide>
+                        <SwiperSlide>4</SwiperSlide>
+                        <SwiperSlide>5</SwiperSlide>
+                        <SwiperSlide>6</SwiperSlide>
+                        <SwiperSlide>7</SwiperSlide>
+                        <SwiperSlide>8</SwiperSlide>
+                        <SwiperSlide>9</SwiperSlide>
                 </Swiper>
             </section>
         </main>
