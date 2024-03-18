2b3f9d26b037e122ba76fe909ad73e5434a22b38
김민준 <hanleejss012@naver.com>
2024-03-18 20:59:51 +0900

    탭 밑줄,swiper개발 초기

--- src/component/header.js
+++ src/component/header.js
@@ -1,31 +1,38 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './header.css';
 
-const linksty = {textDecoration: "none", color:'black'}
+function CustomLink({ to, children }) {
+  const location = useLocation();
+  const isActive = location.pathname === to;
+  const className = isActive ? 'active' : null;
+
+  return <li className={className}><Link to={to}>{children}</Link></li>;
+}
+
 function Header() {
     return (
         <div className="App">
           <header className="App-header">
-              <div class="header-top">
-                <div class="h-t-logo-search">
-                  <div class="header-logo">
-                  <Link to="/" style={linksty}>티켓 로그</Link></div>
-                  <div class="header-search">
-                      <div class="search-form">
+              <div className="header-top">
+                <div className="h-t-logo-search">
+                  <div className="header-logo">
+                  <Link to="/" className="linksty">티켓 로그</Link></div>
+                  <div className="header-search">
+                      <div className="search-form">
                       <input gtm-label="검색창" type="text" placeholder="검색창" value=""/>
-                        <button class="search-btn">
+                        <button className="search-btn">
                           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" viewBox="0 0 20 20"><path stroke="#3A3A3A" stroke-linecap="round" stroke-miterlimit="10" stroke-width="1.6" d="m17.875 17.877-4.607-4.607c-.462-.462-1.198-.56-1.729-.197-1.345.943-3.084 1.356-4.92.943-2.26-.5-4.087-2.328-4.588-4.587A6.157 6.157 0 0 1 8.23 1.876c3.045.098 5.638 2.534 5.923 5.56.079.844-.02 1.66-.245 2.416l-.295.726"></path></svg>
                         </button>
                       </div>
                   </div>
                 </div>
               </div>
-              <div class="header-bottom">
-                <div class="h-b-menu">
+              <div className="header-bottom">
+                <div className="h-b-menu">
                   <ul>             
-                    <li><Link to="/musicall" style={linksty}>뮤지컬</Link></li>
-                    <li><Link to="/consert" style={linksty}>콘서트</Link></li>
+                    <CustomLink to="/musicall">뮤지컬</CustomLink>
+                    <CustomLink to="/consert">콘서트</CustomLink>
                   </ul>
                 </div>
               </div>
--- src/mainpage/main.js
+++ src/mainpage/main.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import './main.css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/autoplay';
 
 function Main() {
     return (
-        <div>
-            메인페이지
-        </div>
+        <main id="contents">
+            <section className="swiper">
+                <Swiper
+                    modules={[Autoplay]}
+                    spaceBetween={50}
+                    slidesPerView={5}
+                    onSlideChange={() => console.log('slide change')}
+                    onSwiper={(swiper) => console.log(swiper)}
+                    autoplay={{ delay: 2000, disableOnInteraction: false }}
+                    loop={true}
+                >
+                    <SwiperSlide>Slide 1</SwiperSlide>
+                    <SwiperSlide>Slide 2</SwiperSlide>
+                    <SwiperSlide>Slide 3</SwiperSlide>
+                    <SwiperSlide>Slide 4</SwiperSlide>
+                    <SwiperSlide>Slide 5</SwiperSlide>
+                    <SwiperSlide>Slide 6</SwiperSlide>
+                    <SwiperSlide>Slide 7</SwiperSlide>
+                    <SwiperSlide>Slide 8</SwiperSlide>
+                    <SwiperSlide>Slide 9</SwiperSlide>
+                </Swiper>
+            </section>
+        </main>
     ); 
 }    
 
