61faa2babdc65b347509bfe181fbe0380a65e450
김민준 <hanleejss012@naver.com>
2024-05-23 18:10:52 +0900

    Font

--- src/mainpage/main/openticket.js
+++ src/mainpage/main/openticket.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './openticket.css';
+import melon from './melon.jpg'
 
 //BySite Active
 function BySite({ to, children, onClick, className }) {
@@ -7,9 +8,18 @@ function BySite({ to, children, onClick, className }) {
 }
 
 export default function Openticket() {
-  
-  const [activeSite, setActiveSite] = useState('all');
+  const [currentDate, setCurrentDate] = useState(new Date());
+  // 월을 이동하는 함수입니다.
+  const moveMonth = (offset) => {
+    // 현재 날짜에 offset 만큼의 월을 더하거나 뺍니다.
+    const newDate = new Date(currentDate.setMonth(currentDate.getMonth() + offset));
+    setCurrentDate(newDate);
+  };
+  // 달력창 띄워주는 로직
+  // const preventDefault = () =>{
 
+  // };
+  const [activeSite, setActiveSite] = useState('all');
   //Site 별 나열 로직
   const handleSiteClick = (site) => {
     setActiveSite(site);
@@ -19,9 +29,14 @@ export default function Openticket() {
   return (
     <div className='openticket-container'>
       <div className='openticket-month'>
-        <button className='month-left'>◀</button>
-        <div className='month'><a href="#none" onClick="">2024.04</a></div>
-        <button className='month-right'>▶</button>
+        <button className='month-left' onClick={() => moveMonth(-1)}>◀</button>
+        {/* 날짜를 YYYY.MM 형식으로 표시합니다. */}
+        <div className='month'>
+          <a href="#none" onClick={(e) => e.preventDefault()}>
+            {currentDate.getFullYear()}.{(currentDate.getMonth() + 1).toString().padStart(2, '0')}
+          </a>
+        </div>
+        <button className='month-right' onClick={() => moveMonth(1)}>▶</button>
       </div>
       <menu className='by-site'>
         <BySite
@@ -74,7 +89,10 @@ export default function Openticket() {
                 <div className='Timer-day'>D-1 12:00</div>
               </div>
               <div className='title'>오픈티켓 제목</div>
-              <div className='day'>2024.4.10</div>
+              <div className='ot-info-bot'>
+                <div className='day'>2024.4.10</div>
+                <div className='tic-site'><img src={melon}></img></div>
+              </div>
             </div>
           </div>  
           );  
