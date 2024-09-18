1a772863531fe3e4d0be9e1fd3d48be2342bcecb
dbwldk <yujiayujia55@naver.com>
2024-09-19 02:45:37 +0900

    검색 페이지 필터 정리
    토글 기능, 버튼 완료
    > 각 필터 모두 보낼 내용(?) 정리 완료
    > 달력은 나중에 시간나면 리셋 기능 넣기(미완료)

--- src/search/filter/filter.js
+++ src/search/filter/filter.js
@@ -4,10 +4,13 @@ import './filter.css';
 
 const Filter = () => {
   /*
-    on-off 기능이 있는 Container에 필요한 변수, 이벤트 함수 설정
+    toggle 기능이 있는 Container에 필요한 변수, 이벤트 함수 설정
    */
     /* 변수 */
-  const [isToggledContainer, setIsToggledContainer] = useState(false); //toggle
+    //toggle
+  const [isToggledTopContainer, setIsToggledTopContainer] = useState(false); //top
+  const [isToggledMidContainer, setIsToggledMidContainer] = useState(false); //Middle
+  const [isToggledBotContainer, setIsToggledBotContainer] = useState(false); //Bottom
 
   //active
   //genre
@@ -40,11 +43,18 @@ const Filter = () => {
   });
 
     /* 이벤트 함수 */
-  //container
-  const toggleContainerHandler = () => {
-    setIsToggledContainer(!isToggledContainer);
+  //toggle container
+  const toggleContainerTopHandler = () => { //top
+    setIsToggledTopContainer(!isToggledTopContainer);
+  };
+  const toggleContainerMidHandler = () => { //Middle
+    setIsToggledMidContainer(!isToggledMidContainer);
+  };
+  const toggleContainerBotHandler = () => { //Bottom
+    setIsToggledBotContainer(!isToggledBotContainer);
   };
 
+  //active item
   const ActiveGenreHandler = (genre) => {
     setIsActiveGenre((prevState) => ({
       ...prevState,
@@ -80,14 +90,13 @@ const Filter = () => {
    */
   //서울, 경기, 인천, 강원, 충남, 충북, 세종, 대전, 경남, 경북, 대구, 울산, 부산, 전남, 전북, 광주, 제주
   const filterRegion = [{value: "서울", class: "Seoul"}, {value: "경기", class: "Gyeonggi"},
-    {value: "인천", class: "Incheon"}, {value: "강원", class: "Gangwon"},
-    {value: "충북", class: "Nchungcheong"}, {value: "충남", class: "Schungcheong"},
-    {value: "세종", class: "Sejong"}, {value: "대전", class: "Daejeon"},
-    {value: "경북", class: "Ngyeongsang"}, {value: "경남", class: "Sgyeongsang"},
-    {value: "대구", class: "Daegu"}, {value: "울산", class: "Ulsan"},
-    {value: "전북", class: "Njeolla"}, {value: "전남", class: "Sjeolla"},
-    {value: "부산", class: "Busan"}, {value: "광주", class: "Gwangju"},
-    {value: "제주", class: "Jeju"}
+    {value: "인천", class: "Incheon"}, {value: "세종", class: "Sejong"}, {value: "강원", class: "Gangwon"},
+    {value: "충청북도", class: "Nchungcheong"}, {value: "충청남도", class: "Schungcheong"},
+    {value: "대전", class: "Daejeon"},{value: "대구", class: "Daegu"},
+    {value: "경상북도", class: "Ngyeongsang"}, {value: "경상남도", class: "Sgyeongsang"},
+    {value: "울산", class: "Ulsan"}, {value: "부산", class: "Busan"},
+    {value: "전라북도", class: "Njeolla"}, {value: "전라남도", class: "Sjeolla"},
+    {value: "광주", class: "Gwangju"}, {value: "제주", class: "Jeju"}
   ];
   const filterRegionBtns = filterRegion.map(item => (
     <button className={`filterBtn ${item.class} ${isActiveRegion[item.class] ? "is-active" : null}`}
@@ -99,65 +108,159 @@ const Filter = () => {
 
 
   /*
-    날짜 str 받아오기
+    결과 배열, str
    */
+  //장르
+  //선택 장르 배열, str
+    //배열
+  const [selectedGenre, setSelectedGenre] = useState([]);
+  useEffect(() => {
+    const selectedGenres = filterGenre
+      .filter(item => isActiveGenre[item.class])
+      .map(item => item.value); //선택된 장르 배열
+    setSelectedGenre(selectedGenres);
+  }, [isActiveGenre]);
+
+    //string:'(첫번째 장르) 외 N개 장르'로 표기 예정
+  const [selectedGenreStr, setSelectedGenreStr] = useState("전체");
+  useEffect(() => {
+    if(selectedGenre.length > 0){ //선택된 장르가 1개 이상
+      const firstGenre = selectedGenre[0]; //첫번째 선택 장르
+      const overGenreLength = selectedGenre.length - 1; //외 장르 개수
+      if(overGenreLength > 0){ //선택 장르 2개 이상
+        setSelectedGenreStr(`${firstGenre} 외 ${overGenreLength}개 장르`);
+      } else { //선택 장르 1개
+        setSelectedGenreStr(firstGenre);
+      }
+    } else { //선택 장르가 없다면
+      setSelectedGenreStr("전체");
+    }
+  }, [selectedGenre]);
+
+  //지역
+  //선택 지역 배열, str
+    //배열
+  const [selectedRegion, setSelectedRegion] = useState([]);
+  useEffect(() => {
+    const selectedRegions = filterRegion
+      .filter(item => isActiveRegion[item.class])
+      .map(item => item.value); //선택된 지역 배열
+      setSelectedRegion(selectedRegions);
+  }, [isActiveRegion]);
+
+    //string:'(첫번째 지역) 외 N개 지역'으로 표기 예정
+  const [selectedRegionStr, setSelectedRegionStr] = useState("전체");
+  useEffect(() => {
+    if(selectedRegion.length > 0){ //선택된 지역이 1개 이상
+      const firstRegion = selectedRegion[0]; //첫번째 선택 지역
+      const overRegionLength = selectedRegion.length - 1; //외 지역 개수
+      if(overRegionLength > 0){ //선택 지역 2개 이상
+        setSelectedRegionStr(`${firstRegion} 외 ${overRegionLength}개 지역`);
+      } else { //선택 지역 1개
+        setSelectedRegionStr(firstRegion);
+      }
+    } else { //선택 지역이 없다면
+      setSelectedRegionStr("전체");
+    }
+  }, [selectedRegion]);
+  
+
+  //날짜 str 받아오기
   const [calendarDateValue, setCalendarDateValue] = useState('');
 
   const handleDateChange = (newDate) => {
     setCalendarDateValue(newDate);
   };
 
+  /*
+  필터 버튼
+  */
+  const ResetBtnHandler = () => { //필터 리셋 버튼
+    // 장르 선택 리셋
+    setIsActiveGenre(prevState => 
+      Object.fromEntries(
+        Object.entries(prevState).map(([genre, _]) => [genre, false])
+      )
+    );
+    // 지역 선택 리셋
+    setIsActiveRegion(prevState => 
+      Object.fromEntries(
+        Object.entries(prevState).map(([region, _]) => [region, false])
+      )
+    );
+
+    //달력 리셋은 복잡할 것 같아서 나중에 시간나면 하겠음.
+  };
+  const SubmitBtnHandler = () => { //필터 적용 버튼
+
+  };
+
   //return
   return (
-    <div className='filter'>
-      {/* filter */}
-      <div className="filterContainer ContainerTop">
-        {/* Container */}
-        <div className='filterHeader'>
-            <h4 className='filterTitle'>장르</h4>
-            <div className='selectedData'>
-              <span className='blind'>선택된 장르:</span>
-              <input type='hidden' name='genre-str' id='genre-str' className='genre-str' value="" />
-            </div>
-        </div>
-        <div className='filterMenu'>
-          {filterGenreBtns}
+    <div className='filterWrap'>
+      <div className='filter'>
+        {/* filter */}
+        <div className={`filterContainer ContainerTop ${isToggledTopContainer ? "is-toggled" : null}`}>
+          {/* Container */}
+          <div className='filterHeader'>
+            <a className='filterToggleBtn' role='button' onClick={toggleContainerTopHandler}>
+              <h4 className='filterTitle'>장르</h4>
+              <div className='selectedData'>
+                <span className='blind'>선택된 장르:</span>
+                <span className='selec-genre'>{selectedGenreStr}</span>
+              </div>
+            </a>
+          </div>
+          <div className='filterMenu'>
+            {filterGenreBtns}
+          </div>
+
         </div>
 
+        <div className={`filterContainer ContainerMiddle ${isToggledMidContainer ? "is-toggled" : null}`}>
+          {/* Container */}
+          <div className='filterHeader'>
+            <a className='filterToggleBtn' role='button' onClick={toggleContainerMidHandler}>
+              <h4 className='filterTitle'>지역</h4>
+              <div className='selectedData'>
+                <span className='blind'>선택된 지역:</span>
+                <span className='selec-region'>{selectedRegionStr}</span>
+              </div>
+            </a>
+          </div>
+          <div className='filterMenu'>
+            {filterRegionBtns}
+          </div>
+
+        </div>
+
+        <div className={`filterContainer ContainerBottom ${isToggledBotContainer ? "is-toggled" : null}`}>
+          {/* Container */}
+          <div className='filterHeader'>
+            <a className='filterToggleBtn' role='button' onClick={toggleContainerBotHandler}>
+              <h4 className='filterTitle'>관람일</h4>
+              <div className='selectedData'>
+                <span className='blind'>선택된 일자:</span>
+                <span className='date'>{calendarDateValue}</span>
+              </div>
+            </a>
+          </div>
+
+          <DatePicker onDateChange={handleDateChange} />
+        </div>
       </div>
-
-      <div className="filterContainer ContainerMiddle">
-        {/* Container */}
-        <div className='filterHeader'>
-            <h4 className='filterTitle'>지역</h4>
-            <div className='selectedData'>
-              <span className='blind'>선택된 지역:</span>
-              <input type='hidden' name='region-str' id='region-str' className='region-str' value="" />
-            </div>
-        </div>
-        <div className='filterMenu'>
-          {filterRegionBtns}
-        </div>
-
-      </div>
-
-      <div className={`filterContainer ContainerBottom ${isToggledContainer ? "is-toggled" : null}`}>
-        {/* Container */}
-        <div className='filterHeader'>
-          <a className='filterToggleBtn' role='button' onClick={toggleContainerHandler}>
-            <h4 className='filterTitle'>관람일</h4>
-            <div className='selectedData'>
-              <span className='blind'>선택된 일자:</span>
-              <span className='date'>{calendarDateValue}</span>
-            </div>
-          </a>
-        </div>
-
-        <DatePicker onDateChange={handleDateChange} />
+      
+      {/* Button */}
+      <div className='filterBtnsWrap'>
+        <button onClick={ResetBtnHandler} className="search-f-reset-btn">
+          초기화
+        </button>
+        <button onClick={SubmitBtnHandler} className="search-f-submit-btn">
+          적용
+        </button>
       </div>
 
     </div>
-    
   );
 };
 export default Filter;
