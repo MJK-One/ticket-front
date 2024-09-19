2630cad4678cb1d1b7b990c4910e23ce39e222e3
dbwldk <yujiayujia55@naver.com>
2024-09-20 02:39:28 +0900

    search Reducer 추가
    검색 관련 reducer 추가
    > spring에 post Mapping으로 넘기고, 결과값 받아올 예정

---
+++ src/store/slice/searchSlice.js
@@ -0,0 +1,48 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+//초기 상태 설정
+const initialState = {
+    search: {
+        genreFilter: [{}],      // 장르 필터 상태
+        regionFilter: [{}],     // 지역 필터 상태
+        period: '',             // 날짜 필터 상태
+        searchKeyword: ''       // 검색어 상태
+    }
+  };
+
+//searchSlice
+const searchSlice = createSlice({
+    name: 'searchs',
+    initialState,
+    reducers: {
+        // genreFilter 업데이트 액션
+        setGenreFilter: (state, action) => {
+            state.search.genreFilter = action.payload;
+        },
+        // regionFilter 업데이트 액션
+        setRegionFilter: (state, action) => {
+            state.search.regionFilter = action.payload;
+        },
+        // period 업데이트 액션
+        setPeriod: (state, action) => {
+            state.search.period = action.payload;
+        },
+        // searchKeyword 업데이트 액션
+        setSearchKeyword: (state, action) => {
+            state.search.searchKeyword = action.payload;
+        },
+        // 검색 필터 모두 초기화
+        clearFilters: (state) => {
+            state.search.genreFilter = [{}];
+            state.search.regionFilter = [{}];
+            state.search.period = '';
+            state.search.searchKeyword = '';
+        }
+    }
+});
+
+// 액션 export
+export const { setGenreFilter, setRegionFilter, setPeriod, setSearchKeyword, clearFilters } = searchSlice.actions;
+
+// 리듀서 export
+export default searchSlice.reducer;
--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import detailReducer from './slice/detailSlice.js';
 import regionReducer from './slice/regionSlice.js';
+import searchReducer from "./slice/searchSlice.js";
 
 const store = configureStore({
     reducer: {
         details: detailReducer,
-        regions: regionReducer
+        regions: regionReducer,
+        searchs: searchReducer
     }
 });
 
